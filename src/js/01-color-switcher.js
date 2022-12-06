aa4e438146aa213a7c453d5b3ca7622be353d368
AlinaBanias <isabella08092001@gmail.com>
2022-12-06 19:06:11 +0200

    Tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const bodyColor = document.body;
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', e => {
+    colorChanger.startColor();
+});
+btnStop.addEventListener('click', e => {
+    colorChanger.stopColor();
+});
+
+
+const colorChanger = {
+    isActive: false,
+    startColor() {
+        if (this.isActive) {
+            return;
+        }
+
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+            bodyColor.style.backgroundColor = getRandomHexColor();
+            console.log(bodyColor.style.backgroundColor)
+        }, 1000);
+        },
+        stopColor() {
+            this.isActive = false;
+            clearInterval(this.intervalId);
+        }
+
+    }
